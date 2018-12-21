ff9918090ee4cb20f6dbbdfdb2ee14ce053314f9
shalk <xshalk@163.com>
2018-12-21 15:39:56 +0800

    update readme

--- download.sh
+++ download.sh
@@ -3,4 +3,4 @@ set -e
 git clone --recursive https://github.com/shalk/hugo-quickstart-demo.git && cd hugo-quickstart-demo && ./docker-run.sh
 echo "Please visit http://localhost:1313 to preview your hugo blog"
 echo ""
-echo "if you want to stop it, run `./docker-run.sh` "
+echo "if you want to stop it, run `./docker-stop.sh' "
