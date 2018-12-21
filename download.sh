6999f26a2f61c85361b06eef90682ac029e7e87a
shalk <xshalk@163.com>
2018-12-21 15:48:40 +0800

    fix escape

--- download.sh
+++ download.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 git clone --recursive https://github.com/shalk/hugo-quickstart-demo.git && cd hugo-quickstart-demo && ./docker-run.sh
+echo ""
 echo "Please visit http://localhost:1313 to preview your hugo blog"
 echo ""
-echo "if you want to stop it, run `./docker-stop.sh' "
+echo "if you want to stop it, run './docker-stop.sh' "
